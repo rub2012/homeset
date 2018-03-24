1753ce2bbfd0cae9cd311e12463dc23374f0d9c8
Julian <>
2018-03-23 22:25:11 -0300

    fix filtro en ajax

--- HomeSet.Web/wwwroot/js/site.js
+++ HomeSet.Web/wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function () {
+
+    $("#gridTabla").on('click', '.paginador', function () {
+        var filtro = $("#filtro").val();
+        var href = $(this).attr('href');
+        $(this).attr('href', href + '&filtro=' + filtro);
+        //return false;
+    });
+
+});
