d8b18f5e8f87579362519349c3daf23997ab3839
julian <julian>
2018-04-11 17:30:20 -0300

    menu y submenu reacomodado

--- HomeSet.Web/wwwroot/js/site.js
+++ HomeSet.Web/wwwroot/js/site.js
@@ -60,7 +60,21 @@
         $("#alertaError").hide();
     });
     
+    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
 
+
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+            $('.dropdown-submenu .show').removeClass("show");
+        });
+
+
+        return false;
+    });
 });
 
 function cargarModalEditar(data) {
