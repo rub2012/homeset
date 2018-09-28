4b92ebdab9ad7e60594a601f70e6cf84dfe19816
Julian Di risio <julian.dirisio@gmail.com>
2018-09-27 23:09:43 -0300

    update config

--- HomeSet.Web/webpack.config.js
+++ HomeSet.Web/webpack.config.js
@@ -1,4 +1,7 @@
-﻿const path = require('path');
+﻿// para actualizar las librerias ejecutar 'npm run build' cada vez que se actualiza las dependencias
+
+
+const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
